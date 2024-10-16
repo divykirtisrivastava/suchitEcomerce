a2e44823fa8529f7491c92b713ea3f14e221b179
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-10-16 17:21:26 +0530

    added

--- backend/databaseConfig.js
+++ backend/databaseConfig.js
@@ -2,7 +2,7 @@ const mysql= require('mysql')
 let connection=mysql.createConnection({
     host: "localhost",
     user:"root",
-    database: "sikha_ecommerce",
+    database: "suchit_ecommerce",
     password:'1234'
 
 })
